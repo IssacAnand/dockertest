e6f57b540d3a8ea2d75f966be9b899c0ce93071a
Issac <issacdread@hotmail.com>
2023-05-24 10:50:59 +0800

    Updated Docker File. Cannot run django

--- fresh/app/views.py
+++ fresh/app/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 # Getting db 
-from.models import Student
+# from.models import Student
 
 # Create your views here.
 
 # Render index.html file
 def index(request):
-    obj= Student.objects.all()
-    context = {
-        "obj": obj,
-    }
-    return render(request,"index.html",context)
+    # obj= Student.objects.all()
+    # context = {
+    #     "obj": obj,
+    # }
+    return render(request,"index.html")
+
+    # return render(request,"index.html")
